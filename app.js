1a5d97b368b4b81b84550505670d1a0a806acc28
Maksim Shelest <shelest.ks@gmai.com>
2022-02-02 17:27:08 +0300

    changed js

--- app.js
+++ app.js
@@ -1,69 +1,46 @@
-const form = document.querySelector('form')
-const input = form.querySelector('.form__input input');
-const todoList = document.querySelector('.todo__list');
-const todoArr = []
-
-// showTodo()
-
-function addTodo() {
-	let value = input.value;
-	if (value) {
-		todoArr.push(value);
-	}
-}
-
-form.addEventListener('submit', (e) => {
-	e.preventDefault()
-	addTodo()
-	showTodo()
-	e.target.reset()
-})
-
-function showTodo() {
-	todoList.innerHTML = ''
-	todoArr.forEach((item, index) => {
-		todoList.innerHTML += `<li class="todo__item"><span>${index + 1} ${item}</span><span class="todo__delete">x</span></li>`
-	})
-	todoList.childNodes.forEach((el, index) => {
-		el.addEventListener('click', (e) => {
-			if (e.target.classList.contains('todo__delete')) {
-				el.remove()
-				todoArr.splice(index, 1)
-				showTodo()
-			}
-		})
-	})
-}
-
-class Todo {
-	constructor(value) {
-		this.value = value
-	}
-
-	createTodo() {
-		if (this.value) {
-			todoArr.push(this.value);
-		}
-		todoList.innerHTML = ''
-		todoArr.forEach((item, index) => {
-			todoList.innerHTML += `<li class="todo__item"><span>${index + 1} ${item}</span><span class="todo__delete">x</span></li>`
-		})
-		deleteTodo();
-	}
-
-	static deleteTodo() {
-		todoList.childNodes.forEach((el, index) => {
-			el.addEventListener('click', (e) => {
-				if (e.target.classList.contains('todo__delete')) {
-					el.remove()
-					todoArr.splice(index, 1)
-					showTodo()
-				}
-			})
-		})
-	}
+document.addEventListener('DOMContentLoaded', () => {
 	
-}
+	const form = document.querySelector('form')
+	const input = form.querySelector('.form__input input');
+	const todoList = document.querySelector('.todo__list');
+	const todoArr = []
+
+	class Todo {
+		constructor(value) {
+			this.value = value
+		}
+
+		addTodo() {
+			if (this.value) {
+				todoArr.push(this.value);
+			}
+		}
+
+		createTodo() {
+			todoList.innerHTML = ''
+			todoArr.forEach((item, index) => {
+				todoList.innerHTML += `<li class="todo__item"><span>${index + 1} ${item}</span><span class="todo__delete">x</span></li>`
+			})
+			todoList.childNodes.forEach((el, index) => {
+				el.addEventListener('click', (e) => {
+					if (e.target.classList.contains('todo__delete')) {
+						el.remove()
+						todoArr.splice(index, 1)
+						this.createTodo()
+					}
+				})
+			})
+		}
+	}
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault()
+		const newTodo = new Todo(input.value)
+		newTodo.addTodo()
+		newTodo.createTodo()
+		e.target.reset()
+	})
+})
 
 
 
