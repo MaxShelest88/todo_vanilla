b07b1f6befdeebc6d3937954dcb0c14c964fab0f
Maksim Shelest <shelest.ks@gmai.com>
2022-02-02 17:10:40 +0300

    class branch

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const form = document.querySelector('form')
 const input = form.querySelector('.form__input input');
-const btn = form.querySelector('.form__btn');
 const todoList = document.querySelector('.todo__list');
 const todoArr = []
 
-showTodo()
+// showTodo()
 
 function addTodo() {
 	let value = input.value;
@@ -13,10 +12,11 @@ function addTodo() {
 	}
 }
 
-btn.addEventListener('click', (e) => {
+form.addEventListener('submit', (e) => {
 	e.preventDefault()
 	addTodo()
 	showTodo()
+	e.target.reset()
 })
 
 function showTodo() {
@@ -35,6 +35,35 @@ function showTodo() {
 	})
 }
 
+class Todo {
+	constructor(value) {
+		this.value = value
+	}
+
+	createTodo() {
+		if (this.value) {
+			todoArr.push(this.value);
+		}
+		todoList.innerHTML = ''
+		todoArr.forEach((item, index) => {
+			todoList.innerHTML += `<li class="todo__item"><span>${index + 1} ${item}</span><span class="todo__delete">x</span></li>`
+		})
+		deleteTodo();
+	}
+
+	static deleteTodo() {
+		todoList.childNodes.forEach((el, index) => {
+			el.addEventListener('click', (e) => {
+				if (e.target.classList.contains('todo__delete')) {
+					el.remove()
+					todoArr.splice(index, 1)
+					showTodo()
+				}
+			})
+		})
+	}
+	
+}
 
 
 
